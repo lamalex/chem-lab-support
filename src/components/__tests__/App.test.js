f5771de694d912a0008909f23a1a532e4baa772d
Alex Launi <alex.launi@gmail.com>
2019-07-16 21:49:40 -0400

    Add more tests

--- src/components/__tests__/App.test.js
+++ src/components/__tests__/App.test.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
 import { mount } from 'enzyme';
+import canvasMock from 'jest-canvas-mock';
 
 import App from '../App';
 
@@ -16,20 +18,43 @@ describe('the qr code generator', () => {
     wrapped = mount(<App />);
   });
 
-  it('renders the react logo svg', () => {
-    expect(wrapped.find('img').length).toEqual(1);
-  });
-
-  it('renders an svg on input', () => {
-    const newValue = 'convert this to a fugging qrcode';
-    wrapped.find('input').simulate('change', {
-      target: {
-        value: newValue
-      }
+  describe('on empty input', () => {
+    it('renders the react logo svg', () => {
+      expect(wrapped.find('img').prop('src')).toMatch(/logo.*svg/);
     });
 
-    wrapped.update();
-    expect(wrapped.find('svg').length).toEqual(1);
+    it('has a disabled download button', () => {
+      expect(wrapped.find('a').hasClass('disabled'));
+    });
+  });
+
+  describe('on text input', () => {
+    beforeEach(() => {
+      const newValue = 'convert this to a fugging qrcode';
+      wrapped.find('input').simulate('change', {
+        target: {
+          value: newValue
+        }
+      });
+
+      wrapped.update();
+    });
+
+    it('renders a canvas', () => {
+      expect(wrapped.find('canvas').length).toEqual(1);
+    });
+
+    it('enables download link', () => {
+      expect(!wrapped.find('a').hasClass('disabled'));
+    });
+
+    describe('on download click', () => {
+      it('sets anchor href', () => {
+        wrapped.find('a').simulate('click');
+        wrapped.update();
+        expect(wrapped.find('a').prop('href')).toBe('data:image/png;base64,00');
+      });
+    });
   });
 
   afterEach(() => {
