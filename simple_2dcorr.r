451d9f20df1a5ecf4388316bb8ad2216fba632ae
Alexander Launi <alex.launi@gmail.com>
2020-07-04 20:02:04 -0400

    Use a real library for argument parsing

--- simple_2dcorr.r
+++ simple_2dcorr.r
@@ -1,22 +1,9 @@
-parse_args <- function(args) {
-    DEFAULT_CONTOUR = 32
-
-    if (is.na(args[1]) || is.na(args[2])) {
-        print("First 2 arguments must be appropriately formatted csv files.")
-        quit(save = "no", status = 69)
-    }
-    
-    contour <- ifelse(is.na(args[3]), DEFAULT_CONTOUR, as.numeric(args[3]))
-    parsed <- list("file1" = args[1], "file2" = args[2], "contour" = contour)
-    return(parsed)
-}
-
 load_csv_to_matrix <- function(file_path) {
     csv <- read.csv(file_path, row.names = 1, check.names = FALSE)
     return(data.matrix(csv, rownames.force = NA))
 }
 
-plot_corr2d_rev <-
+plot_corr2d_custom <-
     function(Obj, what = Re(Obj$FT), specx = Obj$Ref1, specy = Obj$Ref2,
              xlim = NULL, ylim = NULL,
              xlab = expression(nu[1]), ylab = expression(nu[2]),
@@ -134,7 +121,7 @@ plot_corr2d_rev <-
         if (is.null(zlim)) {
             zlim <- range(what[Which1, Which2])
         }
-        # Number of levels is always odd --------------------------------------
+        # Number oxf levels is always odd --------------------------------------
         if (N%%2 == 0){
             N <- N + 1
         }
@@ -219,16 +206,25 @@ plot_corr2d_rev <-
     }
 
 main <- function() {
-    args <- parse_args(commandArgs(trailingOnly = TRUE))
-    mat1 = load_csv_to_matrix(args$file1)
-    mat2 = load_csv_to_matrix(args$file2)
-   
-    library("stringr")
     library("corr2D")
+    library("stringr")
+    library("argparser", quietly = TRUE)
+
+    p <- arg_parser("2D correlation")
+    p <- add_argument(p, "file1", help = "data file 1")
+    p <- add_argument(p, "file2", help = "data file 2")
+    p <- add_argument(p, "--N", help = "Contour level", default = 32, type = "numeric")
+    p <- add_argument(p, "--async", help = "Generate asyncronous plot", flag = TRUE)
+    args <- parse_args(p)
+
+    type <- ifelse(args$async, Im, Re)
+
+    mat1 <- load_csv_to_matrix(args$file1)
+    mat2 <- load_csv_to_matrix(args$file2)
     twod <- corr2d(mat1, mat2, corenumber = parallel::detectCores())
     
-    pdf(str_replace(str_interp("corr2d-${Sys.time()}.pdf"), " ", ""))
-    plot_corr2d_custom(twod, N = args$contour)
+    pdf(str_replace(str_interp("corr2d-${Sys.time()}-${ifelse(args$async, 'async', 'sync')}.pdf"), " ", ""))
+    plot_corr2d_custom(twod, what = type(twod$FT), N = args$N, xlab = "ppm", ylab="ppm")
     dev.off()
 }
 
