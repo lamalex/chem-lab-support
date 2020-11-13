dbde9b0a7036f695b599e5ea4e0417ec5c6c084b
Alexander Launi <alex.launi@gmail.com>
2020-11-12 22:29:17 -0500

    Add TOC curve fitting

---
+++ toc/fit.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+
+import os
+import sys
+import numpy as np
+import pandas as pd
+from scipy.optimize import curve_fit
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+def model_func(t, c1, c2, k1, k2):
+    return c1 * np.exp(-k1*t) + c2 * np.exp(-k2 * t)
+
+
+if __name__ == "__main__":
+    datafile = sys.argv[1]
+
+    if not os.path.exists(datafile):
+        print(f"File `{datafile}' not found")
+        os.exit(69)
+
+
+    X_COL_LABEL = 'Hours'
+    Y_COL_LABEL = 'TOC, norm'
+
+    df = pd.read_csv(datafile)
+
+    x = df[X_COL_LABEL]
+
+    w = []
+    for _ in range(0,2):
+        w = curve_fit(model_func, x, df[Y_COL_LABEL])[0]
+
+    print(f"c1: {w[0]}\nc2:{w[1]}\nk1: {w[2]}\nk2: {w[3]}")
+
+    axs = sns.scatterplot(x=x, y=df[Y_COL_LABEL], markers='g')
+    plt.plot(x, model_func(x, *w), color='r')
+    axs.set_xlim(0, 180)
+    axs.set_ylim(0, 100)
+    plt.show()
